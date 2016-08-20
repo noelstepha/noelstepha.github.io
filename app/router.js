d23da2c8f20da6399863b17d532f5322e95ca3d3
Stephanie Noel <noelstepha@eisti.eu>
2016-08-20 12:01:49 +0200

    change home services

--- app/router.js
+++ app/router.js
@@ -9,13 +9,13 @@ Router.map(function() {
   this.route('home', { path: '/' });
   this.route('home');
   this.route('resume');
-  this.route('resume', { path: '/resume' });
+  this.route('resume', { path: '#resume' });
   this.route('education');
-  this.route('education', { path: '/education' });
+  this.route('education', { path: '#education' });
   this.route('skills');
-  this.route('skills', { path: '/skills' });
+  this.route('skills', { path: '#skills' });
   this.route('contact');
-  this.route('contact', { path: '/contact' });
+  this.route('contact', { path: '#contact' });
 });
 
 export default Router;
